6c11efffddebb516e16d5a44483ca3acd0f9032e
Cory Burmahl <beenters@gmail.com>
2024-09-12 12:02:17 -0700

    Added -m character count

--- aerowc.py
+++ aerowc.py
@@ -12,6 +12,19 @@ def main():
             for line in f.readlines():
                 count += 1
             print(count)
+    if args.w:
+        with open (args.w, 'r') as f:
+            count = 0
+            for line in f.readlines():
+                count += len(line.split())
+            print(count)
+    if args.m:
+        with open (args.m, 'r', encoding='utf-8') as f:
+            count = 0
+            for line in f:
+                count += len(line)
+                count += 1
+            print(count)
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -22,6 +35,8 @@ def parse_args():
 
     c_parser = parser.add_argument('-c', type=str, help='Return Byte Count of File')
     l_parser = parser.add_argument('-l', type=str, help='Return Line Count of File')
+    w_parser = parser.add_argument('-w', type=str, help='Return Word Count of File')
+    m_parser = parser.add_argument('-m', type=str, help='Return Character Count of File')
 
     args = parser.parse_args()
 
