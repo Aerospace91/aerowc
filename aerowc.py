e0071a38e37a39b6b4ce13779986f0307bf6f44f
Cory Burmahl <beenters@gmail.com>
2024-09-12 10:36:07 -0700

    Added ByteCount Functionality

--- aerowc.py
+++ aerowc.py
@@ -1,6 +1,25 @@
+import argparse
+
 def main():
     """Entry point"""
-    print("Hello, World!")
+    args = parse_args()
+    if args.c:
+        with open (args.c, 'rb') as f:
+            print(len(f.read()))
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='Aero WC',
+        description='Clone of wc Command Line Tool',
+        epilog='Have a nice day!'
+    )
+
+    c_parser = parser.add_argument('-c', type=str, help='Return Byte Count of File')
+
+    args = parser.parse_args()
+
+    return args
+
 
 if __name__ == "__main__":
     main()
