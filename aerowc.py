73bbfa8688b3638b4b7301c2a3a75f946b3f7e37
Cory Burmahl <beenters@gmail.com>
2024-09-12 12:10:59 -0700

    Added default no argument functionality

--- aerowc.py
+++ aerowc.py
@@ -25,6 +25,24 @@ def main():
                 count += len(line)
                 count += 1
             print(count)
+    if args.file:
+        string = ""
+        length = 0
+        word = 0
+        byte = 0
+        with open(args.file, 'r') as f:
+            count = 0
+            count2 = 0
+            for line in f.readlines():
+                count += 1
+                count2 += len(line.split())
+            length = count
+            word = count2
+        with open(args.file, 'rb') as f:
+            byte = len(f.read())
+        string = f'{length:7}{word:8}{byte:8} {args.file}'
+        print(string)
+        
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -37,6 +55,7 @@ def parse_args():
     l_parser = parser.add_argument('-l', type=str, help='Return Line Count of File')
     w_parser = parser.add_argument('-w', type=str, help='Return Word Count of File')
     m_parser = parser.add_argument('-m', type=str, help='Return Character Count of File')
+    file_parser = parser.add_argument('file', type=str, nargs='?', help='File to process')
 
     args = parser.parse_args()
 
